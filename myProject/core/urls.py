55c0aa17a4c5a182672b78632e7715370c04a0c6
Md Golam Maula <maula03@gmail.com>
2025-12-04 13:27:57 +0600

    route complete

--- myProject/core/urls.py
+++ myProject/core/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('',views.home,),
     path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
     path('login/', views.login, name='login'),
     path('register/',views.register, name="register"),
 ]
--- myProject/core/views.py
+++ myProject/core/views.py
@@ -10,6 +10,10 @@ def about(request):
     
     return render(request, 'about.html')
 
+def contact(request):
+    
+    return render(request, 'contact.html')
+
 def login(request):
     
     return render(request, 'auth/login.html')
